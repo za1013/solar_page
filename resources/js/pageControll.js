467d4b6ac8a5c7ed1e424000a0580cd376f8ae3e
za1013 <za1013@naver.com>
2025-05-15 15:33:14 +0900

    final version page

--- resources/js/pageControll.js
+++ resources/js/pageControll.js
@@ -20,10 +20,14 @@ new fullpage("#body_container", {
   lazyLoading: true,
 
   afterLoad: (origin, dest, direction) => {
+    const fp_nav = document.getElementById("fp-nav");
     if (dest.anchor === "second_page" || dest.anchor === "footer_page") {
       header.classList.add("black_theme");
+      fp_nav.classList.add("black_theme");
     } else {
       header.classList.remove("black_theme");
+      fp_nav.classList.remove("black_theme");
     }
+    console.log(fp_nav);
   },
 });
